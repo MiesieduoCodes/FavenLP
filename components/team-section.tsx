18bfacc680448058e9196c720ecc8719efc80c3e
ken travis <kentravis37@gmail.com>
2025-05-13 15:45:02 +0100

    update

--- components/team-section.tsx
+++ components/team-section.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import { MoveUpRight } from "lucide-react";
+// import { MoveUpRight } from "lucide-react";
 
 const teamMembers = [
   {
