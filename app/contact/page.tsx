12fd0fa964cacf7392255d12270f3b812238f8d5
ken travis <kentravis37@gmail.com>
2025-05-15 10:47:58 +0100

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,10 @@
-import ContactComponent from "@/components/contactPage"
+import ContactComponent from "@/components/contactPage";
+import { Suspense } from "react";
 
-export default function ContactPage(){
-  return(
-    <ContactComponent/>
-  )
+export default function ContactPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ContactComponent />
+    </Suspense>
+  );
 }
